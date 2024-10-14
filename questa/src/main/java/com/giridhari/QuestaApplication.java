0e1aa3316140dd80a0683c0a257ec61df89e2693
Giridhari <grsb2002@gmail.com>
2024-10-14 13:10:16 +0530

    initial backend structure spring boot

---
+++ questa/src/main/java/com/giridhari/QuestaApplication.java
@@ -0,0 +1,13 @@
+package com.giridhari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestaApplication.class, args);
+	}
+
+}
---
+++ questa/src/test/java/com/giridhari/QuestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giridhari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
