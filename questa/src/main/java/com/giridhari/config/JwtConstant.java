eb47d7491ee0c57a3d4a3b99a762e432f22b0c79
Giridhari <grsb2002@gmail.com>
2024-10-14 17:03:39 +0530

    added intial spring security + jjwt

---
+++ questa/src/main/java/com/giridhari/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.giridhari.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "Giridhari@123";
+    public static final String JWT_HEADER = "Authorization";
+}
