217382822e877525df4a021353cd594ef5a889ca
Giridhari <grsb2002@gmail.com>
2024-10-16 05:08:40 +0530

    added project repository

---
+++ questa/src/main/java/com/giridhari/repository/ProjectRepository.java
@@ -0,0 +1,21 @@
+package com.giridhari.repository;
+
+import com.giridhari.modal.Project;
+import com.giridhari.modal.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProjectRepository extends JpaRepository<Project,Long> {
+
+    List<Project> findByOwner(User user);
+
+    List<Project> findByNameContainingAndTeamContaining(String partialName, User user);
+
+    @Query("SELECT p FROM Project p join p.team t where t=:user")
+    List<Project> findProjectByTeam(@Param("user") User user);
+
+    List<Project> findByTeamContainingOrOwner(User user, User owner);
+}
