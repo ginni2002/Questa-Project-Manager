22c65e7c0d111743ae6c3ed94487f1dc9c1ff0ec
Giridhari <grsb2002@gmail.com>
2024-10-16 05:07:49 +0530

    added user service and implementations

---
+++ questa/src/main/java/com/giridhari/service/UserService.java
@@ -0,0 +1,15 @@
+package com.giridhari.service;
+
+import com.giridhari.modal.User;
+
+public interface UserService {
+
+    User findUserProfileByJwt(String jwt) throws Exception;
+
+    User findUserByEmail(String email) throws Exception;
+
+    User findUserById(Long userId) throws Exception;
+
+    User updateUsersProjectSize(User user, int number) throws Exception;
+
+}
