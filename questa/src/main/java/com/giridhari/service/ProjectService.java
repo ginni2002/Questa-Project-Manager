9ce27649604ed27153ba5470a0afa666f908b1f3
Giridhari <grsb2002@gmail.com>
2024-10-16 04:00:15 +0530

    added project service functions

---
+++ questa/src/main/java/com/giridhari/service/ProjectService.java
@@ -0,0 +1,27 @@
+package com.giridhari.service;
+
+import com.giridhari.modal.Chat;
+import com.giridhari.modal.Project;
+import com.giridhari.modal.User;
+
+import java.util.List;
+
+public interface ProjectService {
+
+    Project createProject(Project project, User user) throws Exception;
+
+    List<Project> getProjectByTeam(User user, String category, String tag) throws Exception;
+
+    Project getProjectById(Long projectId) throws Exception;
+
+    void deleteProject(Long projectId, Long userId) throws Exception;
+
+    Project updatedProject(Project updatedProject, Long id) throws Exception;
+
+    void addUserToProject(Long projectId, Long userId) throws Exception;
+
+    void removeUserToProject(Long projectId, Long userId) throws Exception;
+
+    Chat getChatByProjectId(Long projectId) throws Exception;
+
+}
