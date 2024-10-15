c2aea7e794c461ccd9bb9b3bc15681f4fce9a074
Giridhari <grsb2002@gmail.com>
2024-10-16 03:25:00 +0530

    updated issue schema

--- questa/src/main/java/com/giridhari/modal/Issue.java
+++ questa/src/main/java/com/giridhari/modal/Issue.java
@@ -1,7 +1,12 @@
 package com.giridhari.modal;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -11,7 +16,22 @@ public class Issue {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    private String title;
+    private String description;
+    private String status;
+    private Long projectID;
+    private String priority;
+    private LocalDate dueDate;
+    private List<String> tags = new ArrayList<>();
+
     @ManyToOne
     private User assignee;
 
+    @JsonIgnore
+    @ManyToOne
+    private Project project;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "issue",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comments> comments = new ArrayList<>();
 }
