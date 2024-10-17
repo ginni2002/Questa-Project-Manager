d6a0104573ceae5cfc68e2fdab933a223647b171
Giridhari <grsb2002@gmail.com>
2024-10-17 13:55:16 +0530

    added email service and implementations

---
+++ questa/src/main/java/com/giridhari/service/EmailServiceImpl.java
@@ -0,0 +1,39 @@
+package com.giridhari.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender javaMailSender;
+    @Autowired
+    public EmailServiceImpl(JavaMailSender javaMailSender){
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Override
+    public void sendEmailWithToken(String userEmail, String link) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,"utf-8");
+
+        String subject = "Join Project Team Invitation";
+        String text = "Click the link to join the project team: "+link;
+
+        helper.setSubject(subject);
+        helper.setText(text,true);
+        helper.setTo(userEmail);
+
+        try{
+            javaMailSender.send(mimeMessage);
+        } catch (MailException e){
+            throw new MailSendException("Failed to send email");
+        }
+    }
+}
