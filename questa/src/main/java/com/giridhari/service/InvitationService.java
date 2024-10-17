d1775552eed56763424f3065a98becf95175ce30
Giridhari <grsb2002@gmail.com>
2024-10-17 14:26:26 +0530

    created invitation service and it's implementations

---
+++ questa/src/main/java/com/giridhari/service/InvitationService.java
@@ -0,0 +1,15 @@
+package com.giridhari.service;
+
+import com.giridhari.modal.Invitation;
+import jakarta.mail.MessagingException;
+
+public interface InvitationService {
+
+    public void sendInvitation(String email, Long projectId) throws MessagingException;
+
+    public Invitation acceptInvitation(String token, Long userId) throws Exception;
+
+    public String getTokenByUserMail(String userEmail);
+
+    void deleteToken(String token);
+}
