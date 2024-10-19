dae2665977f37b496c15cfd61cb0a69dda8c5b06
Giridhari <grsb2002@gmail.com>
2024-10-20 00:52:20 +0530

    added subscription related changes in auth controller

--- questa/src/main/java/com/giridhari/controller/AuthController.java
+++ questa/src/main/java/com/giridhari/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.giridhari.repository.UserRepository;
 import com.giridhari.request.LoginRequest;
 import com.giridhari.response.AuthResponse;
 import com.giridhari.service.CustomUserDetailsImpl;
+import com.giridhari.service.SubscriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +34,9 @@ public class AuthController {
     @Autowired
     private CustomUserDetailsImpl customUserDetails;
 
+    @Autowired
+    private SubscriptionService subscriptionService;
+
     @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception{
 
@@ -47,6 +51,8 @@ public class AuthController {
         createdUser.setFullName(user.getFullName());
 
         User savedUser = userRepository.save(createdUser);
+
+        subscriptionService.createSubscription(savedUser);
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword());
         SecurityContextHolder.getContext().setAuthentication(authentication);
