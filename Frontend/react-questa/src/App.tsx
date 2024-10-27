5bfa8bceb9c91bcc3494860680b061d5f7bd8a92
Giridhari <grsb2002@gmail.com>
2024-10-28 01:20:59 +0530

    updated app.tsx by adding basic ts info + home and navbar

--- Frontend/react-questa/src/App.tsx
+++ Frontend/react-questa/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Home from "./pages/home/Home";
+import Navbar from "./pages/navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Home />
     </>
   );
