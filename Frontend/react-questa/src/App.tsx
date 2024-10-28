e2951e0a9e9ee9762f89943815750435aaa4e21d
Giridhari <grsb2002@gmail.com>
2024-10-28 22:33:16 +0530

    added project card

--- Frontend/react-questa/src/App.tsx
+++ Frontend/react-questa/src/App.tsx
@@ -1,12 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
 import Navbar from "./pages/navbar/Navbar";
+import ProjectDetails from "./pages/projectDetails/ProjectDetails";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project/:id" element={<ProjectDetails />} />
+      </Routes>
     </>
   );
 }
--- Frontend/react-questa/src/pages/project/ProjectCard.tsx
+++ Frontend/react-questa/src/pages/project/ProjectCard.tsx
@@ -8,15 +8,23 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { DotFilledIcon, DotsVerticalIcon } from "@radix-ui/react-icons";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 
 const ProjectCard = () => {
+  const navigate: NavigateFunction = useNavigate();
+  const handleNavigate = (projectId: number | string): void => {
+    navigate(`/project/${projectId}`);
+  };
   return (
     <Card className="p-5 w-full lg:max-w-3xl">
       <div className="space-y-5">
         <div className="space-y-2">
           <div className="flex items-center gap-5 justify-between">
             <div className="flex items-center gap-5">
-              <h1 className="cursor-pointer font-bold text-lg">
+              <h1
+                onClick={() => handleNavigate(3)}
+                className="cursor-pointer font-bold text-lg"
+              >
                 Create Ecommerce Project
               </h1>
               <DotFilledIcon />
