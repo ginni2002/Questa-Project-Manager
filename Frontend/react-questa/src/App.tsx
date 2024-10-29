b7c6db3517d608fa7da2795184c6bda4af97810c
Giridhari <grsb2002@gmail.com>
2024-10-30 02:14:50 +0530

    updated final client structure for App

--- Frontend/react-questa/src/App.tsx
+++ Frontend/react-questa/src/App.tsx
@@ -3,16 +3,29 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import Navbar from "./pages/navbar/Navbar";
 import ProjectDetails from "./pages/projectDetails/ProjectDetails";
+import IssueDetails from "./pages/issueDetails/IssueDetails";
+import Subscription from "./pages/subscription/Subscription";
+import Auth from "./pages/auth/Auth";
 
 function App() {
   return (
     <>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/project/:id" element={<ProjectDetails />} />
-      </Routes>
+      {true ? (
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/project/:id" element={<ProjectDetails />} />
+            <Route
+              path="/project/:projectId/issue/:issueId"
+              element={<IssueDetails />}
+            />
+            <Route path="/upgrade-plan" element={<Subscription />} />
+          </Routes>
+        </div>
+      ) : (
+        <Auth />
+      )}
     </>
   );
 }
